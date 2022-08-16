9a0ce76c5e2c6f5a50a7fe87468a1a5929092724
thanhdat5 <meodc94@gmail.com>
2022-08-16 23:42:42 +0700

    Add get 2fa active

--- src/services/appService.ts
+++ src/services/appService.ts
@@ -5,8 +5,12 @@ export const getCurrentUserId = () => {
   return localStorage.getItem(HIMBOX_USER_ID);
 };
 export const getCurrentUser = () => {
-  const userInfo = localStorage.getItem(HIMBOX_USER_INFO) || "";
-  return userInfo ? JSON.parse(userInfo) : null;
+  const userLogged = localStorage.getItem(HIMBOX_USER_INFO) || "";
+  return userLogged ? JSON.parse(userLogged) : null;
+};
+export const is2FAActive = () => {
+  const userLogged = getCurrentUser();
+  return userLogged.tfa || false;
 };
 export const ShowSuccessMessage = (message: string) => {
   toast.success(message);
