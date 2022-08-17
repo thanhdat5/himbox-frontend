8b540980641ae5b261a9a1412b7f771c4034fc9c
thanhdat5 <meodc94@gmail.com>
2022-08-18 00:05:24 +0700

    Update withdraw

--- src/redux/actions/withdrawActions.ts
+++ src/redux/actions/withdrawActions.ts
@@ -1,24 +1,95 @@
-import { WithdrawRequestModel } from "../../models";
 import {
-  WithdrawFailure,
-  WithdrawRequest,
-  WithdrawSuccess,
-  WITHDRAW_FAILURE,
-  WITHDRAW_REQUEST,
-  WITHDRAW_SUCCESS
+  CancelWithdrawRequestModel,
+  ConfirmWithdrawRequestModel,
+  CreateWithdrawRequestModel,
+} from "../../models";
+import {
+  GetListWithdrawFailure,
+  GetListWithdrawRequest,
+  GetListWithdrawSuccess,
+  CreateWithdrawFailure,
+  CreateWithdrawRequest,
+  CreateWithdrawSuccess,
+  ConfirmWithdrawFailure,
+  ConfirmWithdrawRequest,
+  ConfirmWithdrawSuccess,
+  CancelWithdrawFailure,
+  CancelWithdrawRequest,
+  CancelWithdrawSuccess,
+  GET_LIST_WITHDRAW_FAILURE,
+  GET_LIST_WITHDRAW_REQUEST,
+  GET_LIST_WITHDRAW_SUCCESS,
+  CREATE_WITHDRAW_FAILURE,
+  CREATE_WITHDRAW_REQUEST,
+  CREATE_WITHDRAW_SUCCESS,
+  CONFIRM_WITHDRAW_FAILURE,
+  CONFIRM_WITHDRAW_REQUEST,
+  CONFIRM_WITHDRAW_SUCCESS,
+  CANCEL_WITHDRAW_FAILURE,
+  CANCEL_WITHDRAW_REQUEST,
+  CANCEL_WITHDRAW_SUCCESS,
+  ResetWithdrawState,
+  RESET_WITHDRAW_STATE,
 } from "../types/withdraw";
 
-export const withdrawRequest = (
-  payload: WithdrawRequestModel
-): WithdrawRequest => ({
-  type: WITHDRAW_REQUEST,
+export const resetWithdrawState = (): ResetWithdrawState => ({
+  type: RESET_WITHDRAW_STATE,
+});
+
+export const getListWithdrawRequest = (): GetListWithdrawRequest => ({
+  type: GET_LIST_WITHDRAW_REQUEST,
+});
+
+export const getListWithdrawSuccess = (): GetListWithdrawSuccess => ({
+  type: GET_LIST_WITHDRAW_SUCCESS,
+});
+
+export const getListWithdrawFailure = (): GetListWithdrawFailure => ({
+  type: GET_LIST_WITHDRAW_FAILURE,
+});
+
+export const createWithdrawRequest = (
+  payload: CreateWithdrawRequestModel
+): CreateWithdrawRequest => ({
+  type: CREATE_WITHDRAW_REQUEST,
   payload,
 });
 
-export const withdrawSuccess = (): WithdrawSuccess => ({
-  type: WITHDRAW_SUCCESS,
+export const createWithdrawSuccess = (payload: any): CreateWithdrawSuccess => ({
+  type: CREATE_WITHDRAW_SUCCESS,
+  payload,
 });
 
-export const withdrawFailure = (): WithdrawFailure => ({
-  type: WITHDRAW_FAILURE,
+export const createWithdrawFailure = (): CreateWithdrawFailure => ({
+  type: CREATE_WITHDRAW_FAILURE,
+});
+
+export const confirmWithdrawRequest = (
+  payload: ConfirmWithdrawRequestModel
+): ConfirmWithdrawRequest => ({
+  type: CONFIRM_WITHDRAW_REQUEST,
+  payload,
+});
+
+export const confirmWithdrawSuccess = (): ConfirmWithdrawSuccess => ({
+  type: CONFIRM_WITHDRAW_SUCCESS,
+});
+
+export const confirmWithdrawFailure = (): ConfirmWithdrawFailure => ({
+  type: CONFIRM_WITHDRAW_FAILURE,
+});
+
+export const cancelWithdrawRequest = (
+  payload: CancelWithdrawRequestModel
+): CancelWithdrawRequest => ({
+  type: CANCEL_WITHDRAW_REQUEST,
+  payload,
+});
+
+export const cancelWithdrawSuccess = (): CancelWithdrawSuccess => ({
+  type: CANCEL_WITHDRAW_SUCCESS,
+});
+
+export const cancelWithdrawFailure = (): CancelWithdrawFailure => ({
+  type: CANCEL_WITHDRAW_FAILURE,
 });
--- src/redux/saga/withdrawSaga.ts
+++ src/redux/saga/withdrawSaga.ts
@@ -1,10 +1,26 @@
+import { AxiosResponse } from "axios";
 import { all, call, put, takeLatest } from "redux-saga/effects";
+import { ENDPOINTS } from "../../constants";
 import { ShowErrorMessage } from "../../services/appService";
-import { withdrawFailure, withdrawSuccess } from "../actions/withdrawActions";
-import { WITHDRAW_REQUEST } from "../types/withdraw";
+import {
+  cancelWithdrawFailure,
+  cancelWithdrawSuccess,
+  confirmWithdrawFailure,
+  confirmWithdrawSuccess,
+  createWithdrawFailure,
+  createWithdrawSuccess,
+  getListWithdrawFailure,
+  getListWithdrawSuccess
+} from "../actions/withdrawActions";
+import {
+  CANCEL_WITHDRAW_REQUEST,
+  CONFIRM_WITHDRAW_REQUEST,
+  CREATE_WITHDRAW_REQUEST,
+  GET_LIST_WITHDRAW_REQUEST
+} from "../types/withdraw";
 import { apiCall } from "./api";
 
-function* fetchWithdrawSaga(action: any) {
+function* fetchGetListWithdrawSaga(action: any) {
   try {
     yield call(
       apiCall,
@@ -12,15 +28,55 @@ function* fetchWithdrawSaga(action: any) {
       "https://jsonplaceholder.typicode.com/todos",
       action.payload
     );
-    yield put(withdrawSuccess());
+    yield put(getListWithdrawSuccess());
   } catch (e: any) {
-    yield put(withdrawFailure());
+    yield put(getListWithdrawFailure());
+    ShowErrorMessage(e);
+  }
+}
+function* fetchCreateWithdrawSaga(action: any) {
+  try {
+    const res: AxiosResponse<any> = yield call(
+      apiCall,
+      "POST",
+      ENDPOINTS.CREATE_WITHDRAW,
+      action.payload
+    );
+    yield put(createWithdrawSuccess(res.data));
+  } catch (e: any) {
+    yield put(createWithdrawFailure());
+    ShowErrorMessage(e);
+  }
+}
+function* fetchConfirmWithdrawSaga(action: any) {
+  try {
+    yield call(apiCall, "POST", ENDPOINTS.CONFIRM_WITHDRAW, action.payload);
+    yield put(confirmWithdrawSuccess());
+  } catch (e: any) {
+    yield put(confirmWithdrawFailure());
+    ShowErrorMessage(e);
+  }
+}
+function* fetchCancelWithdrawSaga(action: any) {
+  try {
+    yield call(
+      apiCall,
+      "POST",
+      "https://jsonplaceholder.typicode.com/todos",
+      action.payload
+    );
+    yield put(cancelWithdrawSuccess());
+  } catch (e: any) {
+    yield put(cancelWithdrawFailure());
     ShowErrorMessage(e);
   }
 }
 
 function* withdrawSaga() {
-  yield all([takeLatest(WITHDRAW_REQUEST, fetchWithdrawSaga)]);
+  yield all([takeLatest(GET_LIST_WITHDRAW_REQUEST, fetchGetListWithdrawSaga)]);
+  yield all([takeLatest(CREATE_WITHDRAW_REQUEST, fetchCreateWithdrawSaga)]);
+  yield all([takeLatest(CONFIRM_WITHDRAW_REQUEST, fetchConfirmWithdrawSaga)]);
+  yield all([takeLatest(CANCEL_WITHDRAW_REQUEST, fetchCancelWithdrawSaga)]);
 }
 
 export default withdrawSaga;
