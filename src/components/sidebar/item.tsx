5d275d55f9f66c9d2978092f87ea695bbfb23afe
thanhdat5 <meodc94@gmail.com>
2022-08-28 20:30:34 +0700

    Update UI

--- src/components/sidebar/item.tsx
+++ src/components/sidebar/item.tsx
@@ -6,9 +6,10 @@ interface HBSidebarItemProps {
     text: string;
     icon: ReactElement;
     active?: boolean;
+    className?: string;
 }
-const HBSidebarItem = ({ link, text, icon, active }: HBSidebarItemProps) => {
-    return <li className="hb-sidebar-menu-item">
+const HBSidebarItem = ({ link, text, icon, active, className = '' }: HBSidebarItemProps) => {
+    return <li className={`hb-sidebar-menu-item ${className}`}>
         <Link to={link} className={`hb-sidebar-menu-link ${active ? 'active' : ''}`}>
             <span className="hb-sidebar-menu-icon">
                 {icon}
