2801b9c6512e1ad3ffb91a6f03bd8cf0fecd7a69
tungbt <jsmile1994@gmail.com>
2022-09-03 11:05:32 +0700

    update why me

--- src/modules/homepage/sections/why-me.tsx
+++ src/modules/homepage/sections/why-me.tsx
@@ -8,11 +8,13 @@ const WhyChooseHimbox = () => {
             <HBCard className="px-5 py-4">
                 <h2 data-aos="fade-up" className="hb-section-title mb-2">WHY CHOOSE HIMBOX?</h2>
                 <div data-aos="fade-up" data-aos-delay="100" className="hb-section-description mb-5">
-                    <span>An essential component of the Polkadot ecosystem.</span><br/>
-                    <span>The all-in-one platform makes it simple for users to participate in the cryptocurrency market.</span><br/>
-                    <span>Staking DOT with a high APY.</span><br/>
-                    <span>Optimizing earnings using the Smart AMM protocol.</span><br/>
-                    <span>Token HIMBOX's growth potential is correlated with the expansion of the DOT ecosystem and its user base.</span><br/>
+                    <ul>
+                        <li>An essential component of the Polkadot ecosystem.</li>
+                        <li>The all-in-one platform makes it simple for users to participate in the cryptocurrency market.</li>
+                        <li>Staking DOT with a high APY.</li>
+                        <li>Optimizing earnings using the Smart AMM protocol.</li>
+                        <li>Token HIMBOX's growth potential is correlated with the expansion of the DOT ecosystem and its user base.</li>
+                    </ul>
                 </div>
                 <div className="hb-why-channel-image">
                     <Image data-aos="fade-up" data-aos-delay="100" src="/images/why-him.png" alt="" />
