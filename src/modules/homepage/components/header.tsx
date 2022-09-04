437a72e47b3de5fd710c9637cfd80b96080b1eec
thanhdat5 <meodc94@gmail.com>
2022-09-04 08:21:33 +0700

    Update UI mobile

--- src/modules/homepage/components/header.tsx
+++ src/modules/homepage/components/header.tsx
@@ -30,39 +30,15 @@ const PublicHeader = () => {
                     <img src="/images/logo-f.png" alt="HimBOX" />
                 </Link>
                 <div className="hbl-header-actions">
-                    <Button variant="link" type="button" className="hbl-toggle-header">
-                        <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <rect x="2.12134" y="0.0147247" width="21" height="3" rx="1.5"
-                                transform="rotate(45 2.12134 0.0147247)" fill="white"></rect>
-                            <rect y="14.8492" width="21" height="3" rx="1.5" transform="rotate(-45 0 14.8492)"
-                                fill="white"></rect>
-                        </svg>
-                    </Button>
-                    <div className="hbl-header-menu">
-                        {/* <Link className="hbl-header-menu-item" to="/">Home</Link>
-                        <Link className="hbl-header-menu-item" to="/">Documents</Link> */}
-                    </div>
                     <div className="hbl-header-buttons">
                         {
                             logged ? <Link to={ROUTES.DASHBOARD} className="btn btn-primary">Dashboard</Link> : <>
-                                <Link to={ROUTES.SIGN_UP} className="btn btn-primary btn-default">Sign Up</Link>
+                                <Link to={ROUTES.SIGN_UP} className="btn btn-primary btn-default d-none d-md-inline-flex">Sign Up</Link>
                                 <Link to={ROUTES.LOGIN} className="btn btn-primary">Login</Link>
                             </>
                         }
                     </div>
                 </div>
-                <Button variant="link" type="button" className="hbl-toggle-header">
-                    {/* <svg width="21" height="15" viewBox="0 0 21 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <rect width="21" height="3" rx="1.5" fill="white"></rect>
-                        <rect y="6" width="21" height="3" rx="1.5" fill="white"></rect>
-                        <rect y="12" width="21" height="3" rx="1.5" fill="white"></rect>
-                    </svg> */}
-                    {logged ?
-                        <Link to={ROUTES.DASHBOARD} className="btn btn-primary">Dashboard</Link>
-                        :
-                        <Link to={ROUTES.LOGIN} className="btn btn-primary">Login</Link>
-                    }
-                </Button>
             </div>
         </Container>
     </header>
